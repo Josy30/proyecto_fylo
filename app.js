ac8d17b09e3479a1d3e7c2086de0746138491d7b
Joselyn Huaman <j.devetrieva@gmail.com>
2025-06-29 19:15:31 -0500

    actualizacion29/06

---
+++ app.js
@@ -0,0 +1,17 @@
+//funcion que carga las animaciones de las habilidades
+
+function cargarAnimaciones(){
+    $(function(){
+        $('.chart1').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        })
+    })
+}
+
+cargarAnimaciones();
