66bd9654cbd85e75f4d185a085a86472002cdbdd
Joselyn Huaman <j.devetrieva@gmail.com>
2025-07-07 20:45:43 -0500

    actualizacion7/7(1)

--- app.js
+++ app.js
@@ -37,8 +37,53 @@ function efectoHabilidades(){
     const distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         let habilidades = document.getElementsByClassName("chart");
-        habilidades(0).classList.add
+        habilidades(0).classList.add("chart1");
+        habilidades(1).classList.add("chart2");
+        habilidades(2).classList.add("chart3");
+
+        cargarAnimaciones();
     }
 }
 
-cargarAnimaciones();
+//defecto el scrolling para aplicar la animacion de las habilidades
+
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//funcion que muestra el menu resposive
+
+function resposiveMenu(){
+    let x= document.getElementById("nav");
+    if(x.className ===""){
+        x.className = "responsive";
+
+        //creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "x";
+        document.getElementById("nav").appendChild(span);
+
+        //quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
+
+//este codigo es para agregar una funcion a cada liks del menu
+//responsive
+//cuando se presione en cualquiera de los links del menu debe deseparecer el menu
+const link = document.querySelectorAll("#nav a");
+for(i=0; i< links.length;i++){
+    links(i).onclick = function(){
+        const x = document.getElementById('nav');
+        x.className = "";
+
+        //removemos el boton eliminar
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+    }
+}
